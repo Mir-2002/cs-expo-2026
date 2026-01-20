a2f5af153e20897512abaa2c52c3b1198e274a82
Mir-2002 <orfianamir@gmail.com>
2026-01-21 00:23:19 +0800

    Rearranged components

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,6 @@
 "use client";
 import HeroSection from "./components/home/HeroSection";
 import EventsSection from "./components/home/EventsSection";
-import dynamic from "next/dynamic";
 import PartnersSection from "./components/home/PartnersSection";
 
 export default function Home() {
